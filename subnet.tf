385fbfdcaddb2dc1e57cc2375d46169be510918a
root <root@LAPTOP2520.V1.com>
2021-08-24 09:27:28 +0100

    added second subnet

--- subnet.tf
+++ subnet.tf
@@ -11,3 +11,17 @@ resource "oci_core_subnet" "subnet1" {
 output "subnet1"{
     value=oci_core_subnet.subnet1.cidr_block
 }
+
+resource "oci_core_subnet" "subnet2" {
+    cidr_block = "10.0.1.0/24"
+    compartment_id = var.compartment_ocid
+    vcn_id = oci_core_virtual_network.vcn1.id
+    display_name = "subnet2"
+    dns_label = "subnet2"
+    
+  
+}
+
+output "subnet2"{
+    value=oci_core_subnet.subnet2.cidr_block
+}
